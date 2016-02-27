289ff62591f972a46ff17dcd3d0ee340452b60e8
Michael Scully <michael.scully@aya.yale.edu>
2016-02-27 18:30:27 -0500

    working app

--- server.R
+++ server.R
@@ -1,8 +1,23 @@
+# load packages
+library(ggplot2)
+library(datasets)
 library(shiny)
+
+data("Orange", package="datasets")
+
 
 shinyServer(
   function(input,output){
-    
+    output$treeID <- renderText(input$treeID)
+    output$plot <- renderPlot(
+      ggplot(Orange[Orange$Tree==input$treeID,], 
+        aes(Orange[Orange$Tree==input$treeID,]$circumference, 
+            Orange[Orange$Tree==input$treeID,]$age))
+      +geom_point()
+      +geom_smooth()
+      +ylab("Trunk Circumference (mm)")
+      +xlab("Age (days)")
+    )
     
   }
 )
--- ui.R
+++ ui.R
@@ -1,5 +1,45 @@
+# load packages
+library(ggplot2)
+library(datasets)
 library(shiny)
 
+data("Orange", package="datasets")
+treeOptions <- c("Tree #1" = "1",
+                 "Tree #2" = "2",
+                 "Tree #3" = "3",
+                 "Tree #4" = "4",
+                 "Tree #5" = "5")
+
 shinyUI(
+  
+  pageWithSidebar(
+    
+    headerPanel(
+      "Data Products - Shiny Oranges App!"
+         
+                
+    ),
+    
+    sidebarPanel(
+
+      radioButtons(
+        'treeID', "Select Tree", treeOptions),
+      
+      strong("Documentation"),
+      p("This is a super-simple Shiny app that allows you 
+        to  display a plot of age (in days) against trunk 
+        circumference (in mm) for orange trees."),
+      p("Simply select one of the five trees measured above, and the plot
+        will update with the data for that tree."),
+      p("The data used for this project is the 'Orange' data set in the 'datasets' package.")
+
+      
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+    
+  )
+ 
   
 )
